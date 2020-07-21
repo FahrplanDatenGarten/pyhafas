edfce89ef58a3a8ebfed616329eb0ed1caf6319e
Leo Maroni <git@em0lar.de>
2020-07-21 11:50:35 +0200

    Make compatible to python 3.6

--- pyhafas/__init__.py
+++ pyhafas/__init__.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 from .client import HafasClient
 from .types.exceptions import GeneralHafasError, AuthenticationError, AccessDeniedError, LocationNotFoundError, \
     JourneysTooManyTrainsError, JourneysArrivalDepartureTooNearError, TripDataNotFoundError
