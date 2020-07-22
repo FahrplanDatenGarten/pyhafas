8c070f005836ce5359b1088bc538ab81849acf4b
Leo Maroni <git@em0lar.de>
2020-07-22 13:24:38 +0200

    Restructure HafasResponse class
    Co-Authored-By: Simeon Keske <git@n0emis.eu>

--- pyhafas/types/hafas_response.py
+++ pyhafas/types/hafas_response.py
@@ -2,35 +2,44 @@ import json
 
 import requests
 
-from pyhafas.profile.interfaces.mappings.error_codes import ErrorCodesMappingInterface
+from pyhafas.profile.interfaces.mappings.error_codes import \
+    ErrorCodesMappingInterface
 
 
 class HafasResponse:
-    def __init__(self, raw_hafas_response: requests.Response, mapping: ErrorCodesMappingInterface):
+    def __init__(self, raw_hafas_response: requests.Response,
+                 mapping: ErrorCodesMappingInterface):
         data = json.loads(raw_hafas_response.text)
-        self.check_for_errors(data, mapping)
-        self.common = data['svcResL'][0]['res']['common']
-        self.res = data['svcResL'][0]['res']
+        self.raw_hafas_response = raw_hafas_response
+        self.data = data
+        self.check_for_errors(mapping)
 
-    def check_for_errors(self, data: dict, mapping: ErrorCodesMappingInterface):
+    def check_for_errors(self, mapping: ErrorCodesMappingInterface):
         error_not_found = False
-        if data.get('err', "OK") != "OK":
+        if self.data.get('err', "OK") != "OK":
             try:
-                raise mapping[data['err']].value(data.get('errTxt', ''))
+                raise mapping[self.data['err']].value(self.data.get('errTxt', ''))
             except KeyError:
                 error_not_found = True
-
         if error_not_found:
-            raise mapping['default'].value(data.get('errTxt', ''))
+            raise mapping['default'].value(self.data.get('errTxt', ''))
 
-        if not data.get('svcResL', False):
+        if not self.data.get('svcResL', False):
             raise mapping['default'].value("HaFAS response cannot be parsed")
 
-        if data['svcResL'][0].get('err', "OK") != "OK":
+        if self.data['svcResL'][0].get('err', "OK") != "OK":
             try:
-                raise mapping[data['svcResL'][0]['err']].value(data['svcResL'][0].get('errTxt', ''))
+                raise mapping[self.data['svcResL'][0]['err']].value(
+                    self.data['svcResL'][0].get('errTxt', ''))
             except KeyError:
                 error_not_found = True
-
         if error_not_found:
-            raise mapping['default'].value(data['svcResL'][0].get('errTxt', ''))
+            raise mapping['default'].value(self.data['svcResL'][0].get('errTxt', ''))
+
+    @property
+    def common(self):
+        return self.data['svcResL'][0]['res']['common']
+
+    @property
+    def res(self):
+        return self.data['svcResL'][0]['res']
