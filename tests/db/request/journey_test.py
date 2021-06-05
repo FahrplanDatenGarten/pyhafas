4e0cbdf4a312c45e0ab26dd185504d6b26fb679d
Leo Maroni <git@em0lar.de>
2021-06-05 14:32:59 +0200

    Fix journey tests

--- tests/db/request/journey_test.py
+++ tests/db/request/journey_test.py
@@ -8,6 +8,6 @@ from pyhafas.types.fptf import Journey
 def test_db_journey_request():
     client = HafasClient(DBProfile())
     journey = client.journey(
-        journey="¶HKI¶T$A=1@O=Siegburg/Bonn@L=8005556@a=128@$A=1@O=Troisdorf@L=8000135@a=128@$202008081507$202008081512$S     19$$1$$$c",
+        journey="¶HKI¶T$A=1@O=Siegburg/Bonn@L=8005556@a=128@$A=1@O=Hennef(Sieg)@L=8002753@a=128@$202106051440$202106051445$S     12$$1$$$",
     )
     assert isinstance(journey, Journey)
--- tests/vsn/request/journey_test.py
+++ tests/vsn/request/journey_test.py
@@ -6,6 +6,6 @@ from pyhafas.types.fptf import Journey
 def test_vsn_journey_request():
     client = HafasClient(VSNProfile())
     journey = client.journey(
-        journey="¶HKI¶T$A=1@O=Göttingen@L=8000128@a=128@$A=1@O=Lenglern@L=8003644@a=128@$202008090710$202008090719$    RB85$$1$$$",
+        journey="¶HKI¶T$A=1@O=Lenglern Bahnhof@L=9909403@a=128@$A=1@O=Göttingen Bahnhof/ZOB@L=1101000@a=128@$202106051527$202106051550$Bus 220 $$1$$$$",
     )
     assert isinstance(journey, Journey)
