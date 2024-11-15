315e7bbdbebe3fbda6d52bfd90c41020020fe1c7
tiuub <46517077+tiuub@users.noreply.github.com>
2024-11-15 18:57:06 +0100

    Implemented FPTF type Line and Operator. Deprecated Leg.name and Leg.mode, which are now referencing to Leg.line.name and Leg.line.mode.

--- pyhafas/profile/base/helper/parse_leg.py
+++ pyhafas/profile/base/helper/parse_leg.py
@@ -3,7 +3,7 @@ from typing import List
 
 from pyhafas.profile import ProfileInterface
 from pyhafas.profile.interfaces.helper.parse_leg import ParseLegHelperInterface
-from pyhafas.types.fptf import Leg, Mode, Stopover
+from pyhafas.types.fptf import Leg, Mode, Stopover, Line, Operator
 
 
 class BaseParseLegHelper(ParseLegHelperInterface):
@@ -87,9 +87,43 @@ class BaseParseLegHelper(ParseLegHelperInterface):
                                      for msg in stopover.get('msgL', []) if msg.get('remX') is not None]
                         ))
 
+            prodL = common['prodL'][journey['prodX']]
+
+            leg_line = Line(
+                id=str(prodL['name']).lower().replace(' ', '-'),
+                name=prodL['name'],
+                public=True,
+                mode=Mode.TRAIN
+            )
+
+            if 'prodCtx' in prodL:
+                prodCtx = prodL['prodCtx']
+
+                leg_line.fahrtNr = prodCtx['num'] if prodCtx and 'num' in prodCtx else None
+                leg_line.adminCode = prodCtx['admin'] if prodCtx and 'admin' in prodCtx else None
+                leg_line.productName = str(prodCtx['catOut']).strip() if prodCtx and 'catOut' in prodCtx else None
+
+            if 'oprL' in common and 'oprX' in prodL and prodL['oprX'] in common['oprL']:
+                oprL = common['oprL'][prodL['oprX']]
+
+                leg_operator = Operator(
+                    id=str(oprL['name']).lower().replace(' ', '-'),
+                    name=oprL['name']
+                )
+
+                leg_line.operator = leg_operator
+
+            if 'cls' in prodL:
+                byBitmask = {}
+                for availableProducts, bitmasks in self.availableProducts.items():
+                    for bitmask in bitmasks:
+                        byBitmask[bitmask] = availableProducts
+
+                leg_line.product = byBitmask[prodL['cls']]
+
             return Leg(
                 id=journey['jid'],
-                name=common['prodL'][journey['prodX']]['name'],
+                line=leg_line,
                 origin=leg_origin,
                 destination=leg_destination,
                 cancelled=bool(arrival.get('aCncl', False)),
